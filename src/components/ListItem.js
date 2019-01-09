f4b02573777d763529d7b2b7221b7e71bf83d1f7
Mayur Raval <mayurraval@asite.com>
2019-01-09 10:23:15 +0530

    First commit

---
+++ src/components/ListItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
+
+class ListItem extends Component{
+  render(){
+      const { titleStyle } = styles;
+      return(
+        <CardSection >
+          <Text style={titleStyle}>
+            {this.props.library.title}
+          </Text>
+        </CardSection>
+      );
+  }
+}
+
+export default ListItem;
+
+const styles={
+  titleStyle:{
+    fontSize:18,
+    paddingLeft:15,
+    color:'black'
+  }
+}
